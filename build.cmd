d208962b6104fc4ea16fada12eba16e1627f1aeb
CaptainCodeman <simon@captaincodeman.com>
2011-05-31 20:10:29 -0600

    Initial commit

---
+++ build.cmd
@@ -0,0 +1,3 @@
+pkg\nuget\Tools\NuGet i src\Elmah.MongoDB\packages.config -o src\packages
+%windir%\Microsoft.NET\Framework\v3.5\msbuild src\Elmah-MongoDB.sln /t:Rebuild /p:Configuration=Release
+pkg\nuget\pack.cmd
