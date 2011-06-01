58b95b219c0f46b9efdbad366eb2bc706cd481e3
CaptainCodeman <simon@captaincodeman.com>
2011-05-31 20:15:27 -0600

    Path correction on build

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 pkg\nuget\Tools\NuGet i src\Elmah.MongoDB\packages.config -o src\packages
 %windir%\Microsoft.NET\Framework\v3.5\msbuild src\Elmah-MongoDB.sln /t:Rebuild /p:Configuration=Release
-pkg\nuget\pack.cmd
+cd pkg\nuget\
+pack.cmd
