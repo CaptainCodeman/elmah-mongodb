5e8b9ea9799d0c306d1ecdf9753bf842b8682dc3
CaptainCodeman <simon@captaincodeman.com>
2011-10-29 21:59:44 -0600

    Update NuGet

--- pkg/nuget/pack.cmd
+++ pkg/nuget/pack.cmd
@@ -2,4 +2,4 @@ copy ..\..\src\Elmah.MongoDB\bin\Release\Elmah.MongoDB.dll nuspec\lib\net35 /Y
 copy ..\..\src\Elmah.MongoDB\bin\Release\MongoDB.Bson.dll nuspec\lib\net35 /Y
 copy ..\..\src\Elmah.MongoDB\bin\Release\MongoDB.Driver.dll nuspec\lib\net35 /Y
 Tools\NuGet pack nuspec\Elmah.MongoDB.nuspec
-rem Tools\NuGet push  elmah.mongodb.1.2.nupkg
+Tools\NuGet push  elmah.mongodb.1.2.1.nupkg
