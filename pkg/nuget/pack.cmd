1b14f6847982f5adea34fe39ab7574a990cda7bc
CaptainCodeman <simon@captaincodeman.com>
2011-12-27 13:13:53 -0700

    Remove MongoDB driver from package (load from NuGet to avoid versioning issues)

--- pkg/nuget/pack.cmd
+++ pkg/nuget/pack.cmd
@@ -1,4 +1,2 @@
 copy ..\..\src\Elmah.MongoDB\bin\Release\Elmah.MongoDB.dll nuspec\lib\net35 /Y
-copy ..\..\src\Elmah.MongoDB\bin\Release\MongoDB.Bson.dll nuspec\lib\net35 /Y
-copy ..\..\src\Elmah.MongoDB\bin\Release\MongoDB.Driver.dll nuspec\lib\net35 /Y
 ..\..\src\.nuget\NuGet pack nuspec\Elmah.MongoDB.nuspec
