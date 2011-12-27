e6bf73af34ea19993f52ec0c194c970d923068db
CaptainCodeman <simon@captaincodeman.com>
2011-12-27 13:00:25 -0700

    Update to latest NuGet + Elmah + MongoDB driver and bump to v1.3

--- build.cmd
+++
@@ -1,4 +0,0 @@
-pkg\nuget\Tools\NuGet i src\Elmah.MongoDB\packages.config -o src\packages
-%windir%\Microsoft.NET\Framework\v3.5\msbuild src\Elmah-MongoDB.sln /t:Rebuild /p:Configuration=Release
-cd pkg\nuget\
-pack.cmd
